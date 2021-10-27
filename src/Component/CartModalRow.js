a09681fc8a3efe532b678846f56850f43364f681
Niraj Pandey <nirajpandey034@gmail.com>
2021-10-27 10:54:45 +0530

    commiting

--- src/Component/CartModalRow.js
+++ src/Component/CartModalRow.js
@@ -1,11 +1,15 @@
 import React from "react";
+import'../Styles/IncDecButton.css';
 
 //will get item name and price and will return row
 function CartModalRow(props) {
   return (
     <div>
       <div style={{ display: "flex" }}>
-        <p style={{ marginRight: "auto" }}>{props.name} ({props.quantity})</p>
+        <button className="IncDecButton">-</button>
+        <p>{props.quantity}</p>
+        <button className="IncDecButton">+</button>
+        <p style={{ marginRight: "auto" }}>{props.name}</p>
         <p style={{ marginLeft: "auto" }}>Rs.{props.price}</p>
       </div>
       <hr />
